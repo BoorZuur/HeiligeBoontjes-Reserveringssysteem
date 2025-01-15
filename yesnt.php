0f1125c3835cc2f322c3182115ef62e198d0ab32
Luca-K06 <1111360@hr.nl>
2025-01-15 15:02:47 +0100

    Add files via upload

---
+++ yesnt.php
@@ -0,0 +1,38 @@
+<?php
+$host = "144.21.41.225";
+$database = "heilige_boontjes";
+$dbuser = "root";
+$dbpassword = "z0s4PDwLYbvfOd";
+
+$db = mysqli_connect($host, $dbuser, $dbpassword, $database)
+or die ----
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT ---- FROM ----";
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<section class="hero is-small is-warning">
+    <div class="hero-body">
+        <p class="title"> </p>
+        <p class="subtitle"> </p>
+    </div>
+</section>
+// array, tabel location //
+
+</section>
+</body>
+</html>
